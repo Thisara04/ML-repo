aed5ce32b1141f941139941b5d4f13b692c6a53f
Thisara Adhikari <140851451+Thisara04@users.noreply.github.com>
2025-11-08 22:08:10 +0530

    Emphasize 'Shown' in stock price app description

--- new_streamlit_app.py
+++ new_streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.write("""
 # Simple Stock Price App
 
-Shown are the stock **closing price** and ***volume*** of Google!
+*Shown* are the stock **closing price** and ***volume*** of Google!
 
 """)
 
